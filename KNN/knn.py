41cb4a145e07214cef2e051032b51a3899aa5158
shomchak <shomik.chakravarty@gmail.com>
2012-09-18 15:38:34 -0700

    created knn module

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,11 +1,23 @@
-import math
+# A generalized k-nearest-neighbor algorithm for data of arbitrary dimension. 
+# Invoke makeNodeList(data) with properly formatted data to return a NodeList 
+# object. A Node object represents a single data point. Each data point should 
+# have format {'parameter1':value1,'parameter2':value2,...,'type':'type_string'}.
+# To weight the parameters, feed the NodeList.normalizeData(weights) function
+# a dictionary of weights {'parameter1':weight1,'parameter2':weight2,...}. To
+# classify an unknown data point, create a Node object with the unclassified 
+# data, and run NodeList.classify(unkown,k), where k is the number of neighbors
+# desired.
+
+
+
+import math,copy
 
 class Node(object):
     'a k-dimensional data point'
 
     __node_count = 0
 
-    def __init__(self,data,gtype):
+    def __init__(self,data,gtype=None):
         self.data = {}
         for key in data:
             self.data[key] = data[key]
@@ -23,11 +35,15 @@ class NodeList(object):
     
     def __init__(self):
         self.nodes = []
+        self.ranges = {}
+        self.weights = {}
+        self.parameters = []
+        self.normalized = False
 
     def addNode(self,node):
         self.nodes.append(node)
         self.parameters = node.data.keys()
-        self.weights = {}
+        
         for p in self.parameters:
             self.weights[p] = 1
     
@@ -39,7 +55,7 @@ class NodeList(object):
         if len(self.nodes) == 0:
             print "List contains no nodes."
             return
-        self.ranges = {}
+        
         for p in self.parameters:
             p_min = self.nodes[0].data[p]
             p_max = self.nodes[0].data[p]
@@ -58,8 +74,9 @@ class NodeList(object):
             gmax = self.ranges[w]['max']
             for n in self.nodes:
                 n.data[w] = (n.data[w]-gmin)*float(weights[w])/(gmax-gmin)
-            self.ranges[w]['min'] = 0
-            self.ranges[w]['max'] = weights[w]
+            #self.ranges[w]['min'] = 0
+            #self.ranges[w]['max'] = weights[w]
+        self.normalized = True
         print "Normalized."
     
     def distance(self,a,b):
@@ -72,48 +89,22 @@ class NodeList(object):
         if(k<1 or k>len(self.nodes)):
             print "Invalid K."
             return
-        for w in self.weights:
-            gmin = self.ranges[w]['min']
-            gmax = self.ranges[w]['max']
-            unknown.data[w] = (unknown.data[w] - gmin)*float(self.weights[w])/(gmax-gmin)
+        if(self.normalized):
+            unknown = copy.deepcopy(unknown)
+            for w in self.weights:
+                gmin = self.ranges[w]['min']
+                gmax = self.ranges[w]['max']
+                unknown.data[w] = (unknown.data[w] - gmin)*float(self.weights[w])/(gmax-gmin)
         nodes = self.nodes[::]
-        nodes = [x for y,x in sorted(zip([self.distance(a,unknown) for a in nodes],nodes),reverse = True)]
+        nodes = [x for y,x in sorted(zip([self.distance(a,unknown) for a in nodes],nodes))]
         nodes = nodes[0:k]
-        for n in nodes:
-            print n
+        #for n in nodes:
+        #    print n
         tally = {}
         for n in nodes:
             tally[n.type]=tally.setdefault(n.type,0)+1
         gtype = max(tally.items(),key=lambda x: x[1])[0]
         print "Data point is of type %s." % gtype
-
-data = [
-    {'rooms': 1, 'area': 350, 'type': 'apartment'},
-    {'rooms': 2, 'area': 300, 'type': 'apartment'},
-    {'rooms': 3, 'area': 300, 'type': 'apartment'},
-    {'rooms': 4, 'area': 250, 'type': 'apartment'},
-    {'rooms': 4, 'area': 500, 'type': 'apartment'},
-    {'rooms': 4, 'area': 400, 'type': 'apartment'},
-    {'rooms': 5, 'area': 450, 'type': 'apartment'},
-
-    {'rooms': 7,  'area': 850,  'type': 'house'},
-    {'rooms': 7,  'area': 900,  'type': 'house'},
-    {'rooms': 7,  'area': 1200, 'type': 'house'},
-    {'rooms': 8,  'area': 1500, 'type': 'house'},
-    {'rooms': 9,  'area': 1300, 'type': 'house'},
-    {'rooms': 8,  'area': 1240, 'type': 'house'},
-    {'rooms': 10, 'area': 1700, 'type': 'house'},
-    {'rooms': 9,  'area': 1000, 'type': 'house'},
-
-    {'rooms': 1, 'area': 800,  'type': 'flat'},
-    {'rooms': 3, 'area': 900,  'type': 'flat'},
-    {'rooms': 2, 'area': 700,  'type': 'flat'},
-    {'rooms': 1, 'area': 900,  'type': 'flat'},
-    {'rooms': 2, 'area': 1150, 'type': 'flat'},
-    {'rooms': 1, 'area': 1000, 'type': 'flat'},
-    {'rooms': 2, 'area': 1200, 'type': 'flat'},
-    {'rooms': 1, 'area': 1300, 'type': 'flat'},
-] 
 
 def makeNodeList(data):
     myList = NodeList()
